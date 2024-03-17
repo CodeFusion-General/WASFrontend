36f93c870cc1a7984d7b9f871cf7c17ce218e5f1
Kadir Uğurlu <kadir--ugurlu@hotmail.com>
2024-03-17 15:01:10 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx";
+import Slider from "./Slider.jsx";
+
+
+function App() {
+
+    return (
+        <>
+            <Header/>
+            <Slider/>
+        </>
+    )
+}
+
+export default App

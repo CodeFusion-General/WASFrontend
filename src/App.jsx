de267d10a15f5f919bca6b99b1317a17c422f772
Efsitax <kadir--ugurlu@hotmail.com>
2024-03-18 13:30:34 +0300

    Added login page template and router configured.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import Header from "./Header.jsx";
-import Slider from "./Slider.jsx";
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import Header from './Header.jsx';
+import Slider from './MainPage/Slider.jsx';
+import LoginPage from './LoginPage/LoginPage.jsx';
 
+const Dashboard = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    //Pages with header.
+    {
+        path: "/",
+        element: <Dashboard />,
+        children: [
+            {
+                path: "/",
+                element: <Slider />,
+                index: true,
+            },
+        ],
+    },
+    //Pages without header.
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+]);
 
 function App() {
-
     return (
-        <>
-            <Header/>
-            <Slider/>
-        </>
-    )
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
-export default App
+export default App;

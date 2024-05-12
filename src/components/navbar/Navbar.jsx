9a65fd881ffd06eac7c65b21766ca1e3c967ab1f
Şevket Samed Öksüz <s.oksuz1201@hotmail.com>
2024-05-12 17:03:07 +0300

    navbar/sidebar positions are set

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
-import {Fragment, useEffect,useState} from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import {logout, decodeUserToken} from "../../api/authentication/AuthenticationApi.jsx";
-import {getUserPhoto} from "../../api/resource/ResourceApi.jsx";
+import { Fragment, useEffect, useState } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { logout, decodeUserToken } from "../../api/authentication/AuthenticationApi.jsx";
+import { getUserPhoto } from "../../api/resource/ResourceApi.jsx";
+
 const navigation = [
     { name: 'Dashboard', href: '#', current: true },
     { name: 'Team', href: '#', current: false },
     { name: 'Projects', href: '#', current: false },
     { name: 'Calendar', href: '#', current: false },
-]
+];
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-
 function Navbar() {
     const [photo, setPhoto] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     useEffect(() => {
         const decodedToken = decodeUserToken();
         if (decodedToken) {
@@ -32,11 +33,12 @@ function Navbar() {
             }
         }
     }, []);
+
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-gray-900 sticky top-0 z-50 ml-64">
             {({ open }) => (
                 <>
-                    <div className="mx-auto  px-2 sm:px-6 lg:px-8">
+                    <div className="mx-auto px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
@@ -48,16 +50,9 @@ function Navbar() {
                                     ) : (
                                         <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                                     )}
-                                </Disclosure.Button> }
+                                </Disclosure.Button>}
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex flex-shrink-0 items-center">
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                                        alt="Your Company"
-                                    />
-                                </div>
                                 {isLoggedIn && <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
@@ -77,75 +72,75 @@ function Navbar() {
                                 </div>}
                             </div>
                             {isLoggedIn ? <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <button
-                                    type="button"
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                >
-                                    <span className="absolute -inset-1.5" />
-                                    <span className="sr-only">View notifications</span>
-                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                </button>
-
-                                {/* Profile dropdown */}
-                                <Menu as="div" className="relative ml-3">
-                                    <div>
-                                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                            <span className="absolute -inset-1.5" />
-                                            <span className="sr-only">Open user menu</span>
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src={photo || "src/assets/default-user-icon.webp"}
-                                                alt=""
-                                            />
-                                        </Menu.Button>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
+                                    <button
+                                        type="button"
+                                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                     >
-                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                    >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        className={classNames(
-                                                            active ? 'bg-gray-100' : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                        onClick={() => logout()}
-                                                    >
-                                                        Sign out
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
-                            </div> :
+                                        <span className="absolute -inset-1.5" />
+                                        <span className="sr-only">View notifications</span>
+                                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+
+                                    {/* Profile dropdown */}
+                                    <Menu as="div" className="relative ml-3">
+                                        <div>
+                                            <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                                <span className="absolute -inset-1.5" />
+                                                <span className="sr-only">Open user menu</span>
+                                                <img
+                                                    className="h-8 w-8 rounded-full"
+                                                    src={photo || "src/assets/default-user-icon.webp"}
+                                                    alt=""
+                                                />
+                                            </Menu.Button>
+                                        </div>
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-100"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <a
+                                                            href="#"
+                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                        >
+                                                            Your Profile
+                                                        </a>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <a
+                                                            href="#"
+                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                        >
+                                                            Settings
+                                                        </a>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <a
+                                                            className={classNames(
+                                                                active ? 'bg-gray-100' : '',
+                                                                'block px-4 py-2 text-sm text-gray-700'
+                                                            )}
+                                                            onClick={() => logout()}
+                                                        >
+                                                            Sign out
+                                                        </a>
+                                                    )}
+                                                </Menu.Item>
+                                            </Menu.Items>
+                                        </Transition>
+                                    </Menu>
+                                </div> :
                                 <a
                                     href={"/login"}
                                     className={classNames(
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,39 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome, FaRegUser, FaStore, FaUsers, FaBox } from 'react-icons/fa'; // Ensure these are imported for use
 
 const Sidebar = () => {
     return (
-        <div className="fixed top-16 bottom-0 left-0 z-30 w-64
-                        bg-gray-900 text-black rounded-tr-lg rounded-br-lg"> {/* Adjust border-radius here */}
+        <div className="fixed top-0 bottom-0 left-0 z-30 w-64 bg-gray-900 text-gray-100 rounded-br-lg shadow-lg">
+            <div className="flex items-center justify-center p-2 border-b border-gray-700">
+                <img
+                    className="h-12 w-auto"  // Adjusted size for better visibility
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                />
+            </div>
             <nav>
-                <ul className="mt-10">
-                    <li className="px-6 py-2 border-b border-gray-700">
-                        <a href="/" className="text-white hover:text-gray-300">Home</a>
+                <ul className="mt-4">
+                    <li className="px-6 py-2 border-b border-gray-700 flex items-center">
+                        <FaHome className="mr-4"/> {/* Home icon */}
+                        <a href="/" className="flex-grow text-white hover:text-blue-500">Home</a>
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-700">
-                        <a href="/register" className="text-white hover:text-gray-300">Register</a>
+                    <li className="px-6 py-2 border-b border-gray-700 flex items-center">
+                        <FaRegUser className="mr-4"/> {/* Register icon */}
+                        <a href="/register" className="flex-grow text-white hover:text-blue-500">Register</a>
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-700">
-                        <a href="/stores" className="text-white hover:text-gray-300">Stores</a>
+                    <li className="px-6 py-2 border-b border-gray-700 flex items-center">
+                        <FaStore className="mr-4"/> {/* Stores icon */}
+                        <a href="/stores" className="flex-grow text-white hover:text-blue-500">Stores</a>
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-700">
-                        <Link to="/store-employees" className="text-white hover:text-gray-300">Store Employees</Link>
+                    <li className="px-6 py-2 border-b border-gray-700 flex items-center">
+                        <FaUsers className="mr-4"/> {/* Store Employees icon */}
+                        <Link to="/store-employees" className="flex-grow text-white hover:text-blue-500">Store
+                            Employees</Link>
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-700">
-                        <Link to="/product-list" className="text-white hover:text-gray-300">Products</Link>
+                    <li className="px-6 py-2 border-b border-gray-700 flex items-center">
+                        <FaBox className="mr-4"/> {/* Products icon */}
+                        <Link to="/product-list" className="flex-grow text-white hover:text-blue-500">Products</Link>
                     </li>
                 </ul>
             </nav>

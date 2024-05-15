3530968224ad33b3bd0519022dc10535a8571ce5
Yiğit Gürler <yigitgurler@stu.aydin.edu.tr>
2024-05-15 22:16:50 +0300

    Login Page Design changed Sidebar modified to  be responsive logo png added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {FaBox, FaHome, FaRegUser, FaStore, FaUsers} from 'react-icons/fa'; // Ensure these are imported for use
+import { Link } from 'react-router-dom';
+import { FaBox, FaHome, FaRegUser, FaStore, FaUsers } from 'react-icons/fa';
 
 const Sidebar = () => {
     return (
         <div className="fixed top-0 bottom-0 left-0 z-30 w-64 bg-gray-900 text-gray-100 rounded-br-lg shadow-colorful-r">
             <div className="flex items-center justify-center p-2 border-b border-gray-700">
                 <img
-                    className="h-12 w-auto"  // Adjusted size for better visibility
-                    src="src\assets\logowis.png"
+                    className="h-12 w-auto"
+                    src="src/assets/logowis.png"
                     alt="Warehouse Information Systems Logo"
                 />
             </div>
             <nav>
-                <ul className="mt-4">
+                <ul className="mt-4 space-y-2">
                     <li className="px-6 pb-2 border-b border-gray-700 flex items-center">
                         <FaHome className="mr-4"/> {/* Home icon */}
-                        <a href="/" className="flex-grow text-white hover:text-blue-500">Home</a>
+                        <Link to="/" className="flex-grow text-white hover:text-blue-500">Home</Link>
                     </li>
                     <li className="px-6 py-2 border-b border-gray-700 flex items-center">
                         <FaRegUser className="mr-4"/> {/* Register icon */}
-                        <a href="/register" className="flex-grow text-white hover:text-blue-500">Register</a>
+                        <Link to="/register" className="flex-grow text-white hover:text-blue-500">Register</Link>
                     </li>
                     <li className="px-6 py-2 border-b border-gray-700 flex items-center">
                         <FaStore className="mr-4"/> {/* Stores icon */}
-                        <a href="/stores" className="flex-grow text-white hover:text-blue-500">Stores</a>
+                        <Link to="/stores" className="flex-grow text-white hover:text-blue-500">Stores</Link>
                     </li>
                     <li className="px-6 py-2 border-b border-gray-700 flex items-center">
                         <FaUsers className="mr-4"/> {/* Store Employees icon */}
-                        <Link to="/store-employees" className="flex-grow text-white hover:text-blue-500">Store
-                            Employees</Link>
+                        <Link to="/store-employees" className="flex-grow text-white hover:text-blue-500">Store Employees</Link>
                     </li>
                     <li className="px-6 py-2 border-b border-gray-700 flex items-center">
                         <FaBox className="mr-4"/> {/* Products icon */}

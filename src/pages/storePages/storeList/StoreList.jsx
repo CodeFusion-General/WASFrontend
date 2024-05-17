ab60e4e4daca06a6f386763e7187bc7589983af3
Şevket Samed Öksüz <s.oksuz1201@hotmail.com>
2024-05-17 20:16:39 +0300

    Added green background to the StoreList

--- src/pages/storePages/storeList/StoreList.jsx
+++ src/pages/storePages/storeList/StoreList.jsx
@@ -5,6 +5,7 @@ import { GlobalContext } from "../../../api/globalContext/GlobalContext.jsx";
 
 function StoreList() {
     const [stores, setStores] = useState([]);
+    const [selectedStoreId, setSelectedStoreId] = useState(null); // Add state for selected store ID
     const { setGlobalValue } = useContext(GlobalContext);
 
     useEffect(() => {
@@ -51,13 +52,18 @@ function StoreList() {
         });
     };
 
+    const handleSelectStore = (storeId) => {
+        setSelectedStoreId(storeId);
+        setGlobalValue(storeId);
+    };
+
     return (
         <div className="max-w-6xl mx-auto p-5">
             <h1 className="text-3xl font-bold text-center text-gray-800 mb-10">All Stores</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-center">
                 {Array.isArray(stores) && stores.map((store) => (
                     <div key={store.id}
-                         className="bg-white p-6 shadow-lg rounded-lg flex flex-col items-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl border-2 border-gray-300 m-2">
+                         className={`p-6 shadow-lg rounded-lg flex flex-col items-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl border-2 m-2 ${selectedStoreId === store.id ? 'bg-green-200 border-green-600' : 'bg-white border-gray-300'}`}>
                         <h2 className="text-xl font-semibold mb-2">{store.name}</h2>
                         <div className="h-32 w-32 mb-3 overflow-hidden rounded-full border-8 border-gray-200">
                             {store.base64String ? (
@@ -77,7 +83,7 @@ function StoreList() {
                         </div>
                         <p className="text-gray-600 text-sm mb-4">{store.description}</p>
                         <button
-                            onClick={() => setGlobalValue(store.id)}
+                            onClick={() => handleSelectStore(store.id)}
                             className="bg-gray-600 text-white py-2 px-4 rounded hover:bg-gray-900 transition-colors">
                             Select
                         </button>

d4f769cdb2c2ff96fbb6fefd199f242dbe248a08
Efsitax <kadir--ugurlu@hotmail.com>
2024-05-23 22:26:42 +0300

    Functions are updated for translations.

--- src/language/index.jsx
+++ src/language/index.jsx
@@ -1,19 +1,17 @@
+import Cookies from 'js-cookie';
 import resources from "./resources.jsx";
+
 export const translate = (lang, key) => {
-    var translation
-    if (lang){
+    var translation;
+    if (lang) {
         translation = resources[lang][key];
-    }
-    else{
+    } else {
         translation = resources["en"][key];
     }
     return translation;
 };
 
 export const getLanguage = () => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; lng=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-    else return "en";
+    const lang = Cookies.get('lng');
+    return lang || 'en';
 };
-

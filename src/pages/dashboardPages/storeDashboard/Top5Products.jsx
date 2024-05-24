9ca077c3034a8caf483987056314b1bd7f48485e
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-05-24 18:22:07 +0300

    Update DashBoard

--- src/pages/dashboardPages/storeDashboard/Top5Products.jsx
+++ src/pages/dashboardPages/storeDashboard/Top5Products.jsx
@@ -1,14 +1,18 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getTop5MostProfitableProducts } from "../../../api/store/StoreApi";
 import { getLanguage, translate } from "../../../language/index.jsx";
 
 const Top5Products = (props) => {
+  // eslint-disable-next-line react/prop-types
   const { storeId, top = true } = props;
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  const lang = getLanguage();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,7 +22,7 @@ const Top5Products = (props) => {
         setLoading(false);
       } catch (error) {
         setError(
-          `Error fetching top 5 most profitable products: ${error.message}`
+            `${translate(lang, 'errorFetchingCategories')}${error.message}`
         );
         setLoading(false);
       }
@@ -28,7 +32,7 @@ const Top5Products = (props) => {
   }, [storeId, top]);
 
   if (loading) {
-    return <div className="text-center">Loading...</div>;
+    return <div className="text-center">{translate(lang, 'loading')}</div>;
   }
 
   if (error) {
@@ -54,53 +58,53 @@ const Top5Products = (props) => {
   };
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg w-full h-full flex flex-col justify-between">
-      <div>
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          {top
-            ? translate(getLanguage(), "top5MostProfitableProducts")
-            : translate(getLanguage(), "bottom5LeastProfitableProducts")}
-        </h2>
-        <table className="min-w-full bg-white mb-6 border border-gray-200 rounded-lg overflow-hidden shadow-md">
-          <thead className="bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full h-full flex flex-col justify-between">
+        <div>
+          <h2 className="text-2xl font-bold mb-6 text-center">
+            {top
+                ? translate(lang, "top5MostProfitableProducts")
+                : translate(lang, "bottom5LeastProfitableProducts")}
+          </h2>
+          <table className="min-w-full bg-white mb-6 border border-gray-200 rounded-lg overflow-hidden shadow-md">
+            <thead className="bg-gray-100">
             <tr>
               <th className="py-3 px-5 text-left font-semibold text-gray-600">
-                {translate(getLanguage(), "productName")}
+                {translate(lang, "productName")}
               </th>
               <th className="py-3 px-5 text-left font-semibold text-gray-600">
-                {translate(getLanguage(), "profit")}
+                {translate(lang, "profit")}
               </th>
             </tr>
-          </thead>
-          <tbody>
+            </thead>
+            <tbody>
             {products.map((product, index) => (
-              <tr
-                key={index}
-                className={`hover:bg-gray-50 ${getShadowClass(
-                  product.profit,
-                  index
-                )}`}
-              >
-                <td className="py-3 px-5 border-b">{product.name}</td>
-                <td className="py-3 px-5 border-b">
-                  {product.profit >= 0
-                    ? `$${product.profit.toFixed(2)}`
-                    : `-$${Math.abs(product.profit).toFixed(2)}`}
-                </td>
-              </tr>
+                <tr
+                    key={index}
+                    className={`hover:bg-gray-50 ${getShadowClass(
+                        product.profit,
+                        index
+                    )}`}
+                >
+                  <td className="py-3 px-5 border-b">{product.name}</td>
+                  <td className="py-3 px-5 border-b">
+                    {product.profit >= 0
+                        ? `$${product.profit.toFixed(2)}`
+                        : `-$${Math.abs(product.profit).toFixed(2)}`}
+                  </td>
+                </tr>
             ))}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-center">
+          <button
+              onClick={handleViewAllProducts}
+              className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-5 py-3 rounded-lg shadow-md"
+          >
+            {translate(lang, "viewAllProducts")}
+          </button>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <button
-          onClick={handleViewAllProducts}
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-5 py-3 rounded-lg shadow-md"
-        >
-          {translate(getLanguage(), "viewAllProducts")}
-        </button>
-      </div>
-    </div>
   );
 };
 

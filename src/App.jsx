1c97d14d02d5c13d0ba99771d4df057676ebb0ec
Kadir Uğurlu <kadir--ugurlu@hotmail.com>
2024-05-26 22:45:43 +0300

    React lazy load configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,39 @@
+import React, { Suspense, lazy } from 'react';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-//components
-import Navbar from './components/navbar/Navbar.jsx';
-import Sidebar from './components/sidebar/Sidebar.jsx';
-import Footer from './components/footer/Footer.jsx';
-//router
-import BossRouter from "./router/BossRouter.jsx";
-import AdminRouter from "./router/AdminRouter.jsx";
-import ManagerRouter from "./router/ManagerRouter.jsx";
-import EmployeeRouter from "./router/EmployeeRouter.jsx";
-//pages
-import MainPage from './pages/mainPage/MainPage.jsx';
-import Login from './pages/loginPage/LoginPage.jsx';
-import UserAdd from "./pages/userPages/userAdd/UserAdd.jsx";
-import StoreList from './pages/storePages/storeList/StoreList.jsx';
-import StoreAdd from "./pages/storePages/storeAdd/StoreAdd.jsx";
-import StoreEmployee from './pages/storePages/storeEmployee/StoreEmployee.jsx';
-import ProductsList from './pages/productPages/productList/ProductList.jsx'
-import ProductDetail from "./pages/productPages/productDetail/ProductDetail.jsx";
-import ProductAdd from "./pages/productPages/productAdd/ProductAdd.jsx";
-import TransactionList from "./pages/transactionPages/transactionList/TransactionList.jsx";
-import TransactionAdd from "./pages/transactionPages/transactionAdd/TransactionAdd.jsx";
-import TransactionDetails from "./pages/transactionPages/transactionDetails/TransactionDetails.jsx";
-import StoreDashboard from "./pages/dashboardPages/storeDashboard/StoreDashboard.jsx";
-import UserProfile from "./pages/userPages/userProfile/UserProfile.jsx";
-import NotificationList from "./pages/notificationPage/NotificationList.jsx";
-import Settings from "./pages/settingsPage/Settings.jsx";
-import BossDashboard from "./pages/dashboardPages/bossDashboard/BossDashboard.jsx";
-import UserUpdate from "./pages/userPages/userUpdate/UserUpdate.jsx";
-import CompanyAdd from "./pages/companyPages/companyAdd/CompanyAdd.jsx";
-import CompanyDetail from "./pages/companyPages/companyDetail/CompanyDetail.jsx";
-import CompanyList from "./pages/companyPages/companyList/CompanyList.jsx";
-import CompanyUpdate from "./pages/companyPages/companyUpdate/CompanyUpdate.jsx";
-import CategoryList from "./pages/categoryPages/CategoryList.jsx";
-//global
-import {GlobalStoreIdProvider} from "./api/store/GlobalStoreId.jsx";
-import {GlobalCompanyIdProvider} from "./api/company/GlobalCompanyId.jsx";
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Footer from './components/footer/Footer';
+import BossRouter from "./router/BossRouter";
+import AdminRouter from "./router/AdminRouter";
+import ManagerRouter from "./router/ManagerRouter";
+import EmployeeRouter from "./router/EmployeeRouter";
+import { GlobalStoreIdProvider } from "./api/store/GlobalStoreId";
+import { GlobalCompanyIdProvider } from "./api/company/GlobalCompanyId";
 
-
+// Dinamik importlar
+const MainPage = lazy(() => import('./pages/mainPage/MainPage'));
+const Login = lazy(() => import('./pages/loginPage/LoginPage'));
+const UserAdd = lazy(() => import("./pages/userPages/userAdd/UserAdd"));
+const StoreList = lazy(() => import('./pages/storePages/storeList/StoreList'));
+const StoreAdd = lazy(() => import("./pages/storePages/storeAdd/StoreAdd"));
+const StoreEmployee = lazy(() => import('./pages/storePages/storeEmployee/StoreEmployee'));
+const ProductsList = lazy(() => import('./pages/productPages/productList/ProductList'));
+const ProductDetail = lazy(() => import("./pages/productPages/productDetail/ProductDetail"));
+const ProductAdd = lazy(() => import("./pages/productPages/productAdd/ProductAdd"));
+const TransactionList = lazy(() => import("./pages/transactionPages/transactionList/TransactionList"));
+const TransactionAdd = lazy(() => import("./pages/transactionPages/transactionAdd/TransactionAdd"));
+const TransactionDetails = lazy(() => import("./pages/transactionPages/transactionDetails/TransactionDetails"));
+const StoreDashboard = lazy(() => import("./pages/dashboardPages/storeDashboard/StoreDashboard"));
+const UserProfile = lazy(() => import("./pages/userPages/userProfile/UserProfile"));
+const NotificationList = lazy(() => import("./pages/notificationPage/NotificationList"));
+const Settings = lazy(() => import("./pages/settingsPage/Settings"));
+const BossDashboard = lazy(() => import("./pages/dashboardPages/bossDashboard/BossDashboard"));
+const UserUpdate = lazy(() => import("./pages/userPages/userUpdate/UserUpdate"));
+const CompanyAdd = lazy(() => import("./pages/companyPages/companyAdd/CompanyAdd"));
+const CompanyDetail = lazy(() => import("./pages/companyPages/companyDetail/CompanyDetail"));
+const CompanyList = lazy(() => import("./pages/companyPages/companyList/CompanyList"));
+const CompanyUpdate = lazy(() => import("./pages/companyPages/companyUpdate/CompanyUpdate"));
+const CategoryList = lazy(() => import("./pages/categoryPages/CategoryList"));
 
 const Dashboard = () => {
     return (
@@ -62,152 +59,194 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <MainPage />,
+                element: <Suspense fallback={<div>Loading...</div>}><MainPage /></Suspense>,
                 index: true,
             },
             {
                 path: "/employee-register",
-                element: <BossRouter
-                    element={<UserAdd type="Employee" />}
-                />,
+                element: (
+                    <BossRouter>
+                        <Suspense fallback={<div>Loading...</div>}><UserAdd type="Employee" /></Suspense>
+                    </BossRouter>
+                ),
             },
             {
                 path: "/register",
-                element: <AdminRouter
-                    element={<UserAdd type="Boss"/>}
-                />,
+                element: (
+                    <AdminRouter>
+                        <Suspense fallback={<div>Loading...</div>}><UserAdd type="Boss" /></Suspense>
+                    </AdminRouter>
+                ),
             },
             {
                 path: "/add-store",
-                element: <BossRouter
-                    element={<StoreAdd />}
-                />,
+                element: (
+                    <BossRouter>
+                        <Suspense fallback={<div>Loading...</div>}><StoreAdd /></Suspense>
+                    </BossRouter>
+                ),
             },
             {
                 path: "/stores",
-                element: <BossRouter
-                    element={<StoreList />}
-                />,
+                element: (
+                    <BossRouter>
+                        <Suspense fallback={<div>Loading...</div>}><StoreList /></Suspense>
+                    </BossRouter>
+                ),
             },
             {
                 path: "/store-employees",
-                element: <ManagerRouter
-                    element={<StoreEmployee />}
-                />,
+                element: (
+                    <ManagerRouter>
+                        <Suspense fallback={<div>Loading...</div>}><StoreEmployee /></Suspense>
+                    </ManagerRouter>
+                ),
             },
             {
                 path: "/product-list",
-                element: <EmployeeRouter
-                    element={<ProductsList />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><ProductsList /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/product-list/category/:categoryId",
-                element: <EmployeeRouter
-                    element={<ProductsList
-                        type="category"
-                    />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><ProductsList type="category" /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/product-details/:productId",
-                element: <EmployeeRouter
-                    element={<ProductDetail />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><ProductDetail /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/add-product",
-                element: <EmployeeRouter
-                    element={<ProductAdd />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><ProductAdd /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/transactions/:productId",
-                element: <EmployeeRouter
-                    element={<TransactionList />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><TransactionList /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/add-transaction/:productId",
-                element: <EmployeeRouter
-                    element={<TransactionAdd />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><TransactionAdd /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/transaction-details/:transactionId",
-                element: <TransactionDetails />,
+                element: <Suspense fallback={<div>Loading...</div>}><TransactionDetails /></Suspense>,
             },
             {
                 path: "/store",
-                element: <EmployeeRouter
-                    element={<StoreDashboard />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><StoreDashboard /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/boss-dashboard",
-                element: <BossRouter
-                    element={<BossDashboard />}
-                />,
+                element: (
+                    <BossRouter>
+                        <Suspense fallback={<div>Loading...</div>}><BossDashboard /></Suspense>
+                    </BossRouter>
+                ),
             },
             {
                 path: "/profile",
-                element: <EmployeeRouter
-                    element={<UserProfile />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><UserProfile /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/notifications",
-                element: <EmployeeRouter
-                    element={<NotificationList />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><NotificationList /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/edit-profile",
-                element: <EmployeeRouter
-                    element={<UserUpdate />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><UserUpdate /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/settings",
-                element: <EmployeeRouter
-                    element={<Settings />}
-                />,
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><Settings /></Suspense>
+                    </EmployeeRouter>
+                ),
             },
             {
                 path: "/new-company/:bossId",
-                element: <AdminRouter
-                    element={<CompanyAdd />}
-                />,
+                element: (
+                    <AdminRouter>
+                        <Suspense fallback={<div>Loading...</div>}><CompanyAdd /></Suspense>
+                    </AdminRouter>
+                ),
             },
             {
                 path: "/companies",
-                element: <AdminRouter
-                    element={<CompanyList />}
-                />,
+                element: (
+                    <AdminRouter>
+                        <Suspense fallback={<div>Loading...</div>}><CompanyList /></Suspense>
+                    </AdminRouter>
+                ),
             },
             {
                 path: "/company-detail",
-                element: <BossRouter
-                    element={<CompanyDetail />}
-                />,
+                element: (
+                    <BossRouter>
+                        <Suspense fallback={<div>Loading...</div>}><CompanyDetail /></Suspense>
+                    </BossRouter>
+                ),
             },
             {
                 path: "/company-update",
-                element: <AdminRouter
-                    element={<CompanyUpdate />}
-                />,
+                element: (
+                    <AdminRouter>
+                        <Suspense fallback={<div>Loading...</div>}><CompanyUpdate /></Suspense>
+                    </AdminRouter>
+                ),
             },
             {
                 path: "/categories",
-                element: <EmployeeRouter
-                    element={<CategoryList />}
-                />
-            }
+                element: (
+                    <EmployeeRouter>
+                        <Suspense fallback={<div>Loading...</div>}><CategoryList /></Suspense>
+                    </EmployeeRouter>
+                ),
+            },
         ],
     },
     {
         path: "/login",
-        element: <Login />,
+        element: <Suspense fallback={<div>Loading...</div>}><Login /></Suspense>,
     },
 ]);
 
@@ -216,7 +255,7 @@ function App() {
         <div className="App">
             <GlobalStoreIdProvider>
                 <GlobalCompanyIdProvider>
-                <RouterProvider router={router} />
+                    <RouterProvider router={router} />
                 </GlobalCompanyIdProvider>
             </GlobalStoreIdProvider>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        // Eğer birden fazla giriş noktası varsa, onları burada belirtebilirsiniz
+        // about: 'about.html',
+        // contact: 'contact.html'
+      },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Tüm node_modules dosyalarını ayrı bir chunk içine al
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Chunk boyutu uyarı limitini artırmak için
+  },
+  base: './', // Bu ayar, dosya yollarının doğru çalışmasını sağlar
+});
